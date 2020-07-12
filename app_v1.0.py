4be0c7eb12c81e9a57c0530a34ed24966615716e
Shakib Rahman <shr382@mail.usask.ca>
2020-07-12 04:27:22 -0600

    Added documentation

--- app_v1.0.py
+++ app_v1.0.py
@@ -1,6 +1,6 @@
 import os
 
-
+# read the target files
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 target_file = open(os.path.join(__location__, "targets.txt"), 'r')
 targets_string = target_file.read()
@@ -8,6 +8,12 @@ target_file.close()
 
 
 def get_targets(in_string):
+	"""
+	Split the targets and arrange them into a set and a dictionary
+
+	:param in_string: input string taken from targets text file
+	:return: a tuple of a set of single word targets and a dictionary of multi word targets
+	"""
 	sw_set = set()
 	mw_dict = dict()
 	word = ""
@@ -42,25 +48,33 @@ def get_targets(in_string):
 
 
 sw_targets, mw_targets = get_targets(targets_string)
-catch_continuous = True
+catch_continuous = True                                # this is for catching continuous terms
 
-
+# read the input file
 in_raw_file = open(os.path.join(__location__, "bee-movie-script.txt"), 'r')
 in_file_string = in_raw_file.read()
 in_raw_file.close()
 
 
-def calc_coordinates(in_string):
+def calc_coordinates(in_string, sw_set, mw_dict):
+	"""
+	Finds the location of the target words in in_string
+
+	:param mw_dict: a dictionary containing multiple word turgets
+	:param sw_set: a set containing single word targets
+	:param in_string: the input string
+	:return: a list of (tuples of) coordinates
+	"""
 	coordinate_list = list()
 	word = ""
 	for i in range(len(in_string)):
 		if in_string[i].isalpha():
 			word += in_string[i].lower()
 		else:
-			if (word in sw_targets) or (word[-3:] == "ing" and catch_continuous):
+			if (word in sw_set) or (word[-3:] == "ing" and catch_continuous):
 				coordinate_list.append((i-len(word), len(word)))
-			elif word in mw_targets:
-				for target_word in mw_targets[word]:
+			elif word in mw_dict:
+				for target_word in mw_dict[word]:
 					spotlight_word = in_string[i:i+len(target_word)]
 					spotlight_word.replace('\n', ' ')
 					if target_word == spotlight_word:
@@ -69,7 +83,8 @@ def calc_coordinates(in_string):
 	return coordinate_list
 
 
-coordinates = calc_coordinates(in_file_string)
+# prepare for coloring
+coordinates = calc_coordinates(in_file_string, sw_targets, mw_targets)
 latest = 0
 out_string = ""
 for i in range(len(coordinates)):
@@ -77,4 +92,4 @@ for i in range(len(coordinates)):
 	out_string += "\033[44;33m" + in_file_string[coordinates[i][0]:coordinates[i][0] + coordinates[i][1]] + "\033[m"
 	latest = coordinates[i][0] + coordinates[i][1]
 out_string += in_file_string[latest:]
-print(out_string)
+print(out_string)                                      # single print function for output
